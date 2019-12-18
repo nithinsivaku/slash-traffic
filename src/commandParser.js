d4be14e81e955e4f09996b515b2c26e13cf5fdca
Nithin Sivakumar <nithinsivakumar@outlook.com>
2019-12-17 22:03:23 -0500

    refactor doc

--- src/commandParser.js
+++ src/commandParser.js
@@ -1,7 +1,6 @@
 /**
  * commandParser is probably the core module of our project. 
  * It has the goal to take an arbitrary string of text/ 2 strings
- * and extract location and distance informations for googleAPI.
+ * and extract origin and destination informations for slashcommand to process.
  */
 
- 
